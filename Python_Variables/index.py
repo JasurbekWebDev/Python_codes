cc6f4732cfec6e8c841667077d61b91f23a5a3ff
JasurbekWebDev <168339759+JasurbekWebDev@users.noreply.github.com>
2025-09-10 21:14:41 +0900

    Update index.py

---
+++ Python_Variables/index.py
@@ -0,0 +1,65 @@
+# 2. O'zgaruvchilarni e'lon qilish
+# ozgaruvchi_nomi = qiymat
+
+# Turli xil ma'lumot turlari:
+
+# Raqamli ma'lumotlar:
+# Butun sonlar (int)
+
+yosh = 25
+soni = 100
+print(yosh)
+print(soni)
+
+# O'nlik sonlar (float)
+narx = 15.99
+pi = 3.14159
+print(narx)
+print(pi)
+# Kompleks sonlar (complex)
+z = 3 + 4j
+print(z)
+
+
+# Matnli ma'lumotlar:
+
+# String (str)
+ism = "Ahmad"
+familya = "Karimov"
+manzil = """Toshkent shahri,
+Mirzo Ulug'bek tumani"""
+print(ism)
+print(familya)
+print(manzil)
+
+# Mantiqiy ma'lumotlar:
+# Boolean (bool)
+togri = True
+notogri = False
+print(togri)
+print(notogri)
+
+# Maxsus qiymat:
+# None
+bosh_qiymat = None
+print(bosh_qiymat)
+
+# Bir vaqtda bir nechta o'zgaruvchi e'lon qilish:
+# Bir xil qiymat
+a = b = c = 10
+print(a)
+print(b)
+print(c)
+
+# Har xil qiymatlar
+x, y, z = 1, 2, 3
+ismlar = ism, yosh, shahar = "Ali", 30, "Samarqand"
+print(x)
+print(y)
+print(z)
+
+print(ism)
+print(yosh)
+print(shahar)
+print(ismlar)
+
